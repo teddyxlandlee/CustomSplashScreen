9d3bb480a1abf1915af11a7de22acf8805795c60
Teddy Li <teddyxlandlee@hotmail.com>
2022-08-12 07:14:03 +0800

    Update dependencies Add debug code

--- CustomSplashScreenF2F/src/main/java/eu/midnightdust/customsplashscreen/f2f/SplashOverlayTransform.java
+++ CustomSplashScreenF2F/src/main/java/eu/midnightdust/customsplashscreen/f2f/SplashOverlayTransform.java
@@ -1,14 +1,20 @@
 package eu.midnightdust.customsplashscreen.f2f;
 
 import cpw.mods.modlauncher.api.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
+import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.tree.ClassNode;
 import xland.mcmodbridge.fa2fomapper.SupportedPlatform;
 import xland.mcmodbridge.fa2fomapper.api.Mapping;
 import xland.mcmodbridge.fa2fomapper.api.tiny.TinyUtils;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.StringReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
@@ -47,7 +53,23 @@ public class SplashOverlayTransform implements ITransformationService {
             @Override
             public @NotNull ClassNode transform(ClassNode input, ITransformerVotingContext context) {
                 AsmSplashScreen scr = new AsmSplashScreen(mapping::mapField, mapping::mapMethod);
-                return scr.apply(input);
+                var o = scr.apply(input);
+                if ("true".equals(System.getProperty("fa2fomapper.export"))) {
+                    new Thread(() -> {
+                        ClassWriter cw = new ClassWriter(1);
+                        o.accept(cw);
+                        var p = Paths.get(".fa2fomapper", o.name + ".class");
+                        try {
+                            Files.createDirectories(p.getParent());
+                            try (var os = Files.newOutputStream(p)) {
+                                os.write(cw.toByteArray());
+                            }
+                        } catch (IOException e) {
+                            LOGGER.fatal("Can't dump class {}", o.name, e);
+                        }
+                    }, "SPLASH-OVERLAY-TRANS-2").start();
+                }
+                return o;
             }
 
             @Override
@@ -59,6 +81,8 @@ public class SplashOverlayTransform implements ITransformationService {
             public @NotNull Set<Target> targets() {
                 return Collections.singleton(Target.targetClass(type));
             }
+
+            private static final Logger LOGGER = LogManager.getLogger();
         });
     }
 }
